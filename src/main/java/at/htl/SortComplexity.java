5f32a9b5c6e58e413236a487b8fce67da8608033
schollsebastian <scholl.sebastian@gmx.at>
2019-04-05 00:00:16 +0200

    finished

--- src/main/java/at/htl/SortComplexity.java
+++ src/main/java/at/htl/SortComplexity.java
@@ -1,29 +1,72 @@
 package at.htl;
 
+import java.util.Scanner;
+
 public class SortComplexity {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfArrays;
+        int length;
+        float average = 0;
 
+        System.out.print("Anzahl der zu sortierenden Arrays: ");
+        numberOfArrays = scanner.nextInt();
 
+        System.out.print("Größe der Arrays: ");
+        length = scanner.nextInt();
+        System.out.println();
+
+        for (int i = 0; i < numberOfArrays; i++) {
+            long time = sortRandomArray(length);
+
+            System.out.printf("Laufzeit zur Sortierung des %d. Arrays: %d ms%n", i + 1, time);
+
+            average = (average * i + time) / (i + 1);
+        }
+
+        System.out.printf("%nDurchschnittliche Laufzeit: %.2f ms", average);
     }
 
     // Sorts an array of random numbers and returns the sorting run time in ms
     public static long sortRandomArray(int length) {
-        return 0;
+        int[] array = generateRandomArray(length);
+        long time = System.currentTimeMillis();
+
+        sort(array);
+
+        return System.currentTimeMillis() - time;
     }
 
     // Returns an array of random numbers
     public static int[] generateRandomArray(int length) {
-        return null;
+        int[] array = new int[length];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100 + 1);
+        }
+
+        return array;
     }
 
     // Sorts an array of numbers in ascending order
     public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
 
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[j - 1] > array[j]) {
+                    swap(array, j - 1, j);
+                }
+
+            }
+        }
     }
 
     // Swaps the array elements with index i and j
     public static void swap(int[] array, int i, int j) {
-
+        int help = array[i];
+        array[i] = array[j];
+        array[j] = help;
     }
 }
